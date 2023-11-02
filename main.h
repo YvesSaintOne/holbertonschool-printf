550048d8ea3ec540b7749f910cd2e083746fe7aa
root <onterrionw@gmail.com>
2023-11-02 15:53:52 -0700

    printf.c updates

--- main.h
+++ main.h
@@ -1,5 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
-int _putchar(char c);
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
+
+int _print(const char *format, ...);
 
 #endif
---
+++ printf.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _printf - to print formatted text to the standard output stream
+ *@format: input strings
+ * Return: the number of characters printed
+ */
+
+int _printf(const char *format, ...);
+{
+	
