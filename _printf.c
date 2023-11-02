c53d02a67a35b562629262ec4bb9ecb077305a3d
root <onterrionw@gmail.com>
2023-11-02 14:27:53 -0700

    working on _printf.c file

---
+++ _printf.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _printf - a set of functions that take a format string as input
+ *
+ * c - prints a single character
+ *
+ * Retrun: the number of characters printed
+ */
+int _printf(const char *format, ...)
+{
